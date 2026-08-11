b07a636d952306fa1560df2cafa2c3ea503bcff1
sojibulislamrana <sojibulislamrana.dev@gmail.com>
2026-08-11 13:59:49 +0600

    feat: bullet list order and undorder list

--- src/app/documents/layout.tsx
+++ src/app/documents/layout.tsx
@@ -2,13 +2,8 @@ interface DocumentLayoutProps {
   children: React.ReactNode;
 }
 
-const DocumentsLayout = ({children} : DocumentLayoutProps) => {
-  return (
-    <div className="flex flex-col gap-y-4">
-      <div className="w-full bg-red-800">Document Navbar</div>
-      {children}
-    </div>
-  );
-}
- 
+const DocumentsLayout = ({ children }: DocumentLayoutProps) => {
+  return <div className="flex flex-col gap-y-4">{children}</div>;
+};
+
 export default DocumentsLayout;
