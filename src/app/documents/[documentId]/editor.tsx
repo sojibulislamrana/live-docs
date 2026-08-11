19a1ede83a5d3f568ff696280ef690305d518d8b
sojibulislamrana <sojibulislamrana.dev@gmail.com>
2026-08-11 21:16:39 +0600

    feat: toolbar bold, italic, underline, comment, tasklist, remove mark , etc.

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -8,6 +8,7 @@ import Table from "@tiptap/extension-table";
 import TableRow from "@tiptap/extension-table-row";
 import TableCell from "@tiptap/extension-table-cell";
 import TableHeader from "@tiptap/extension-table-header";
+import { Underline } from "@tiptap/extension-underline";
 import Image from "@tiptap/extension-image";
 import ImageResize from "tiptap-extension-resize-image";
 import { useEditorStore } from "@/store/use-editor-store";
@@ -50,6 +51,7 @@ export const Editor = () => {
     },
     extensions: [
       StarterKit,
+      Underline,
       Table,
       TableHeader,
       TableRow,
