9ee6a5c0b2c0d815cbda32a20a359ef522c65e28
sojibulislamrana <sojibulislamrana.dev@gmail.com>
2026-09-20 17:26:45 +0600

    feat: toolbar insert table

--- src/app/documents/[documentId]/navbar.tsx
+++ src/app/documents/[documentId]/navbar.tsx
@@ -36,8 +36,17 @@ import {
   Undo2Icon,
 } from "lucide-react";
 import { BsFilePdf } from "react-icons/bs";
+import { useEditorStore } from "@/store/use-editor-store";
 
 export const Navbar = () => {
+  const { editor } = useEditorStore();
+  const insertTable = ({ rows, cols }: { rows: number; cols: number }) => {
+    editor
+      ?.chain()
+      .focus()
+      .insertTable({ rows, cols, withHeaderRow: false })
+      .run();
+  };
   return (
     <nav className="flex items-center justify-between">
       <div className="flex gap-2 items-center">
@@ -103,11 +112,19 @@ export const Navbar = () => {
                 </MenubarTrigger>
 
                 <MenubarContent>
-                  <MenubarItem>
+                  <MenubarItem
+                    onClick={() => {
+                      editor?.chain().focus().undo().run();
+                    }}
+                  >
                     <Undo2Icon className="size-4 mr-2" />
                     Undo <MenubarShortcut>⌘Z</MenubarShortcut>
                   </MenubarItem>
-                  <MenubarItem>
+                  <MenubarItem
+                    onClick={() => {
+                      editor?.chain().focus().redo().run();
+                    }}
+                  >
                     <Redo2Icon className="size-4 mr-2" />
                     Redo <MenubarShortcut>⌘R</MenubarShortcut>
                   </MenubarItem>
@@ -124,10 +141,34 @@ export const Navbar = () => {
                       Table
                     </MenubarSubTrigger>
                     <MenubarSubContent>
-                      <MenubarItem>1 x 1</MenubarItem>
-                      <MenubarItem>2 x 2</MenubarItem>
-                      <MenubarItem>3 x 3</MenubarItem>
-                      <MenubarItem>4 x 4</MenubarItem>
+                      <MenubarItem
+                        onClick={() => {
+                          insertTable({ rows: 1, cols: 1 });
+                        }}
+                      >
+                        1 x 1
+                      </MenubarItem>
+                      <MenubarItem
+                        onClick={() => {
+                          insertTable({ rows: 2, cols: 2 });
+                        }}
+                      >
+                        2 x 2
+                      </MenubarItem>
+                      <MenubarItem
+                        onClick={() => {
+                          insertTable({ rows: 3, cols: 3 });
+                        }}
+                      >
+                        3 x 3
+                      </MenubarItem>
+                      <MenubarItem
+                        onClick={() => {
+                          insertTable({ rows: 4, cols: 4 });
+                        }}
+                      >
+                        4 x 4
+                      </MenubarItem>
                     </MenubarSubContent>
                   </MenubarSub>
                 </MenubarContent>
