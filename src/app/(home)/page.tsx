938bd80b4f2f61fb9a984a93df901c86da9e0cd9
sojibulislamrana <sojibulislamrana.dev@gmail.com>
2026-09-24 11:49:48 +0600

    db: convex database setup

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,9 +1,17 @@
+"use client";
+
 import Link from "next/link";
 import React from "react";
 import { Navbar } from "./navbar";
 import { TemplateGallery } from "./template-gallary";
+import { useQuery } from "convex/react";
+import { api } from "../../../convex/_generated/api";
 
 const Home = () => {
+  const documents = useQuery(api.document.get);
+  if (documents === undefined) {
+    return <p>Loading....</p>;
+  }
   return (
     <div className=" min-h-screen flex flex-col">
       <div className="fixed top-0 left-0 right-0 z-10 h-16 bg-white p-4">
@@ -11,6 +19,9 @@ const Home = () => {
       </div>
       <div className="mt-16">
         <TemplateGallery />
+        {documents?.map((document) => (
+          <span key={document._id}>{document.title}</span>
+        ))}
       </div>
     </div>
   );
